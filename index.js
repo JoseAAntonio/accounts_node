8200c838f64bd3e061b866c2d4d746ab33fe49a6
Jose Antonio <antoniojaraujo0@gmail.com>
2023-02-05 15:54:24 -0300

    Finalização da operação de saque com atualização do novo saldo

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import fs from "fs";
 
 operations();
 
+//NOTE - menu inicial com as opções de escolha do usuário
 function operations() {
 	inquirer
 		.prompt([
@@ -31,7 +32,9 @@ function operations() {
 			} else if (action === "Depositar") {
 				deposit();
 			} else if (action === "Consultar saldo") {
+				getAccountBalance();
 			} else if (action === "Sacar") {
+				withdraw();
 			} else if (action === "Sair") {
 				console.log(chalk.bgBlue.black("Obrigado por usar nossos serviços"));
 				process.exit();
@@ -99,8 +102,9 @@ function buildAccount() {
 		.catch((err) => console.error(err));
 }
 
-//NOTE - Make a deposit on an existing account
+//NOTE - Fazer um deposito numa conta existente
 function deposit() {
+	//prompt para o usuário digitar a conta para deposito
 	inquirer
 		.prompt([
 			{
@@ -108,29 +112,34 @@ function deposit() {
 				message: "Digite o nome da sua conta",
 			},
 		])
+		//tratando a resposta do usuário
 		.then((answer) => {
 			const accountName = answer["accountName"];
 			if (!checkAccounts(accountName)) {
 				return deposit();
 			}
+			//prompt para o usuario escolher o valor do deposito
 			inquirer
 				.prompt([
 					{
 						name: "amount",
-						message: "Qunato você deseja depositar?",
+						message: "Quanto você deseja depositar?",
 					},
 				])
+				//tratando a resposta do usuário adicionando o valor a sua conta
 				.then((answer) => {
 					const amount = answer["amount"];
 
 					addAmount(accountName, amount);
 					operations();
 				})
+				//tratamento de possiveis erros
 				.catch((err) => console.log(err));
 		})
 		.catch((err) => console.log(err));
 }
 
+//NOTE - helper function para adicionar valores a conta
 function addAmount(accountName, amount) {
 	const accountData = getAccount(accountName);
 	console.log(accountData);
@@ -155,6 +164,7 @@ function addAmount(accountName, amount) {
 	);
 }
 
+//NOTE - helper function que retorna os dados da conta para tratamento
 function getAccount(accountName) {
 	const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
 		encoding: "utf8",
@@ -162,4 +172,85 @@ function getAccount(accountName) {
 	});
 
 	return JSON.parse(accountJSON);
+}
+
+//NOTE - Consultar saldo
+function getAccountBalance() {
+	inquirer
+		.prompt([
+			{
+				name: "accountName",
+				message: "Por favor Digite o nome da sua conta",
+			},
+		])
+		.then((answer) => {
+			const accountName = answer["accountName"];
+
+			if (!checkAccounts(accountName)) {
+				return getAccountBalance();
+			}
+
+			const accountData = getAccount(accountName);
+
+			console.log(
+				chalk.bgBlue.white(`Seu saldo é de R$${accountData.balance}`)
+			);
+			operations();
+		})
+		.catch((err) => console.error(err));
+}
+
+//NOTE - Sacar valores da conta
+function withdraw() {
+	inquirer
+		.prompt([
+			{
+				name: "accountName",
+				message: "Digite o nome da sua conta",
+			},
+		])
+		.then((answer) => {
+			const accountName = answer["accountName"];
+
+			if (!checkAccounts(accountName)) {
+				return withdraw();
+			}
+
+			inquirer
+				.prompt([
+					{
+						name: "amount",
+						message: "Digite o valor do saque",
+					},
+				])
+				.then((answer) => {
+					const amount = answer["amount"];
+					removeAmount(accountName, amount);
+				})
+				.catch((err) => console.error(err));
+		})
+		.catch((err) => console.error(err));
+}
+
+function removeAmount(accountName, amount) {
+	const accountData = getAccount(accountName);
+
+	if (!amount) {
+		console.log(chalk.bgRed.white("Por favor digite um valor válido"));
+		return withdraw();
+	}
+	if (accountData.balance < amount) {
+		console.log(chalk.bgRed.white("Valor indisponivel"));
+		return withdraw();
+	}
+	accountData.balance = parseFloat(accountData.balance) - parseFloat(amount);
+	fs.writeFileSync(
+		`accounts/${accountName}.json`,
+		JSON.stringify(accountData),
+		function (err) {
+			console.error(err);
+		}
+	);
+	console.log(chalk.bgGreen.white(`Saque realizado com sucesso no valor de R$${amount}`));
+	operations();
 }
